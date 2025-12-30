ae899b991e3e46d860441f285fb6395c1ead5462
Goremagon <whogormagon@gmail.com>
2025-12-29 18:51:39 -0800

    cleaned up and addded files
    added tailwind config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:8000",
+      '/api': {
+        target: 'http://127.0.0.1:8000', // Points to Python backend
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' prefix
       },
     },
   },
-});
+})
